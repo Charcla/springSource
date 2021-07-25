27fcc90b93c43bd6dd7e8d026962afd18d9fa080
Charcla <skyou001@163.com>
2021-07-25 19:37:28 +0800

    首次提交

---
+++ spring-context/src/test/java/test/MyBeanPostPro.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBeanPostPro implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		return bean;
+	}
+}
